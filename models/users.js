c5d222c116d3ea8d9ce5d858fbce7ed622518218
Ian <ianleeschneider@gmail.com>
2024-05-22 20:17:44 -0400

    removed unique key from User model

--- models/users.js
+++ models/users.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const UserSchema = Schema({
-    email: { type: String, required: true, unique: true },
+    email: { type: String, required: true },
     password: {type: String, required: true},
     firstName:  {type: String, required: true},
     lastName: {type: String, required: false},
