40f56361d955fa2f5d0c33beaebb60efa659d204
Ian <ianleeschneider@gmail.com>
2024-05-27 00:32:36 -0400

    fixed unique key by adding index key for MongoDB Atlas

--- models/users.js
+++ models/users.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const UserSchema = Schema({
-    email: { type: String, unique:true, required: true },
+    email: { type: String, required: true, unique: true, index: true},
     password: {type: String, required: true},
     firstName:  {type: String, required: true},
     lastName: {type: String, required: false},
